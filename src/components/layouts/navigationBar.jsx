fc8f5856ce7fbf37f80ff100bf7c0773fea930cf
haelim <gpfla7375@gmail.com>
2024-06-19 10:20:46 +0900

    24_06_19 10:21 네비바 수정 모달관련 및 임포트관련

--- src/components/layouts/navigationBar.jsx
+++ src/components/layouts/navigationBar.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Navbar, Nav, Container, NavDropdown, Button, Modal } from "react-bootstrap";
 import { observer } from "mobx-react";
-import Link from "next/link";
 import { authStore } from "../../stores/authStore";
 import { logout } from "../../api/user";
 
+
 const NavigationBar = observer(() => {
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
   const loggedIn = authStore.loggedIn;
 
   useEffect(() => {
@@ -17,10 +18,13 @@ const NavigationBar = observer(() => {
     logout().then((res) => {
       localStorage.clear();
       authStore.setLoggedIn(false);
+      setShowLogoutModal(false); //모달달기
+      window.location.href="/"; //로그아웃 후 메인 페이지로 이동
     });
   };
 
   return (
+    <>
     <Navbar
       collapseOnSelect
       expand="lg"
@@ -29,127 +33,134 @@ const NavigationBar = observer(() => {
       className="navbar"
     >
       <Container>
-        <Navbar.Brand>
-          <Link href="/main" legacyBehavior>
-            <a>
-              <img
-                src="/image/Logo.png"
-                alt="Logo"
-                style={{ width: "110px" }}
-              />
-            </a>
-          </Link>
+        <Navbar.Brand href="/">
+          <img src="/image/Logo.png" alt="Logo" style={{ width: "110px" }} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <nav className="navigation-container">
             <ul style={{ marginBottom: "0px" }}>
               <li>
-                <Link href="/resume" legacyBehavior>
-                  <a style={{ marginRight: "10px" }}>이력서 매니저</a>
-                </Link>
+                <a href="/resume">이력서 매니저</a>
                 <div className="submenu">
-                  <Link href="/resume/MyResumeInsert" legacyBehavior>
-                    <a className="subword">이력서 작성</a>
-                  </Link>
+                  <a className="subword" href="/resume/MyResumeInsert">
+                    이력서 작성
+                  </a>
                   <br />
-                  <Link href="/resume/MyResume" legacyBehavior>
-                    <a className="subword">이력서 관리</a>
-                  </Link>
+                  <a className="subword" href="/resume/MyResume">
+                    이력서 관리
+                  </a>
                   <br />
-                  <Link href="/resume/MyResume" legacyBehavior>
-                    <a className="subword">합격자 이력서 공유</a>
-                  </Link>
+                  <a className="subword" href="/resume/MyResume">
+                    합격자 이력서 공유
+                  </a>
                   <br />
-                  <Link href="/resume/MyResume" legacyBehavior>
-                    <a className="subword">합격자 키워드 분석</a>
-                  </Link>
+                  <a className="subword" href="/resume/MyResume">
+                    합격자 키워드 분석
+                  </a>
                   <br />
                 </div>
               </li>
               <li>
-                <Link href="/interview" legacyBehavior>
-                  <a>AI 면접</a>
-                </Link>
+                <a href="/interview">AI 면접</a>
                 <div className="submenu">
-                  <Link href="/interview/test" legacyBehavior>
-                    <a className="subword">모의면접</a>
-                  </Link>
+                  <a className="subword" href="/interview/test">
+                    모의면접
+                  </a>
                   <br />
-                  <Link href="/Interview/list" legacyBehavior>
-                    <a className="subword">면접 report</a>
-                  </Link>
+                  <a className="subword" href="/interviewlist">
+                    면접 report
+                  </a>
                   <br />
                 </div>
               </li>
               <li>
-                <Link href="/search" legacyBehavior>
-                  <a>AI 트랜드서칭</a>
-                </Link>
+                <a href="/search">AI 트랜드서칭</a>
               </li>
               <li>
-                <Link href="/companylist" legacyBehavior>
-                  <a>기업공고</a>
-                </Link>
+                <a href="/companylist">기업공고</a>
               </li>
               <li>
-                <Link href="/community" legacyBehavior>
-                  <a>커뮤니티</a>
-                </Link>
+                <a href="/community">커뮤니티</a>
               </li>
               <li>
-                <Link href="/notice" legacyBehavior>
-                  <a>공지사항</a>
-                </Link>
+                <a href="/notice">공지사항</a>
               </li>
               <li>
-                <Link href="/manager" legacyBehavior>
-                  <a>관리자</a>
-                </Link>
+                <a href="/manager">관리자</a>
                 <div className="submenu">
-                  <Link href="/manager/Dashboard" legacyBehavior>
-                    <a className="subword">대시보드</a>
-                  </Link>
+                  <a className="subword" href="/manager/Dashboard">
+                    대시보드
+                  </a>
                   <br />
-                  <Link href="/manager/UserManagement" legacyBehavior>
-                    <a className="subword">회원리스트</a>
-                  </Link>
+                  <a className="subword" href="/manager/UserManagement">
+                    회원리스트
+                  </a>
                   <br />
-                  <Link href="/manager/AdminManagement" legacyBehavior>
-                    <a className="subword">관리자리스트</a>
-                  </Link>
+                  <a className="subword" href="/manager/AdminManagement">
+                    관리자리스트
+                  </a>
                   <br />
-                  <Link href="/manager/ReportList" legacyBehavior>
-                    <a className="subword">신고리스트</a>
-                  </Link>
+                  <a className="subword" href="/manager/ReportList">
+                    신고리스트
+                  </a>
                   <br />
                 </div>
               </li>
             </ul>
           </nav>
 
+          {/* <Nav className="me-auto">
+            <Nav.Link href={"/resume"} className="nav-link">
+              이력서 매니저
+            </Nav.Link>
+            <Nav.Link href={"/interview"} className="nav-link">
+              AI 면접
+            </Nav.Link>
+            <Nav.Link href={"/search"} className="nav-link">
+              AI 트랜드 서칭
+            </Nav.Link>
+            <Nav.Link href={"/companylist"} className="nav-link">
+              기업공고
+            </Nav.Link>
+            <Nav.Link href={"/community"} className="nav-link">
+              커뮤니티
+            </Nav.Link>
+            <Nav.Link href={"/notice"} className="nav-link">
+              공지사항
+            </Nav.Link>
+          </Nav> */}
           {loggedIn ? (
             <Nav>
-              <Nav.Link onClick={handleLogout}>로그아웃</Nav.Link>
-              <Nav.Link>
-                <Link href="/" legacyBehavior>
-                  내 정보
-                </Link>
-              </Nav.Link>
+              <Nav.Link onClick={() => setShowLogoutModal(true)}>로그아웃</Nav.Link>
+              <Nav.Link href={"/member/myinfo"}>내 정보</Nav.Link>
             </Nav>
           ) : (
             <Nav>
-              <Nav.Link href="/member/login" legacyBehavior>
-                로그인
-              </Nav.Link>
-              <Nav.Link href="/member" legacyBehavior>
-                회원가입
-              </Nav.Link>
+              <Nav.Link href={"/member/login"}>로그인</Nav.Link>
+              <Nav.Link href={"/member"}>회원가입</Nav.Link>
             </Nav>
           )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    {/* 모달 확인 */}
+    <Modal className="custom-modal" show={showLogoutModal} onHide={() => setShowLogoutModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>로그아웃 확인</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>정말 로그아웃 하시겠습니까?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowLogoutModal(false)}>
+              취소
+            </Button>
+            <Button variant="primary" onClick={handleLogout}>
+              로그아웃
+            </Button>
+          </Modal.Footer>
+    </Modal>
+    </>
   );
 });
 
